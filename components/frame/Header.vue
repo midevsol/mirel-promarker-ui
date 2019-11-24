35045394fc2d82728a4a96c9bafd79de6276870e
ve <ve@vemic.net>
2019-11-24 17:40:41 +0900

    delete unnecessary styles

--- components/frame/Header.vue
+++ components/frame/Header.vue
@@ -1,22 +1,4 @@
 <template>
-  <!--
-  <header id="mui-global-wrap">
-    <div class="mui-global-logo-wrap" />
-    <div>
-      <nav id="mui-global-navi">
-        <ul>
-          <router-link class="mui-global-navi-item" tag="li" to="/" exact>
-            <a>TOP</a>
-          </router-link>
-          <router-link class="mui-global-navi-item" tag="li" to="/mste" exact>
-            <a>MSTE</a>
-          </router-link>
-        </ul>
-      </nav>
-    </div>
-    <div class="mui-global-util-wrap" />
-  </header>
-  -->
   <header>
     <b-navbar type="light" variant="faded">
       <b-navbar-brand href="#">
@@ -33,10 +15,9 @@
           </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
-
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Command Palatte" />
+          <b-form-input size="sm" class="mr-sm-4" placeholder="Command" />
           <b-button size="sm" class="my-2 my-sm-0" type="submit" hidden />
         </b-nav-form>
 
@@ -58,47 +39,3 @@ export default {
   name: 'CommonHeader'
 }
 </script>
-
-<style lang="css" scoped>
-#mui-global-wrap {
-  display:flat;
-}
-
-#mui-header {
-  height:30px;
-  display:flat;
-  justify-content:space-around;
-  margin:0;
-}
-
-.mui-global-navi-item {
-  width:60px ;
-  text-align:center;
-  padding:20px;
-  margin:0;
-}
-
-.mui-global-navi-item a {
-  color:white;
-}
-
-.mui-global-navi-item:hover {
-  background-color:#000000;
-  opacity:0.5;
-}
-
-#mui-global-navi {
-  list-style: none;
-  padding: 0;
-  background-color: rgba(220, 53, 53, 0.89);
-}
-#mui-global-navi li {
-  display: inline-block;
-  padding: 5px;
-}
-
-.mui-global-logo-wrap {
-  width: 300px;
-}
-
-</style>
