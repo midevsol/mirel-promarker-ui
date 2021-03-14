d58c2aaedaca2d194d69e988af32381385e2a843
vemi <ve@vemic.net>
2021-03-14 18:53:36 +0900

    add AppRunner

---
+++ pages/apprunner/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="container_title">
+      AppRunner
+    </div>
+    <div class="inner">
+      <div class="rightitems">
+        <b-button :disabled="disabled || processing" variant="secondary">
+          📄全てクリア
+        </b-button>
+        <b-button v-b-modal.modal-psv-dialog :disabled="disabled || processing" variant="secondary">
+          📎Yaml形式
+        </b-button>
+      </div>
+      <hr>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+// import BvUpload from '~/components/dialog/BvDownloadDialog.vue'
+
+export default {
+  layout: 'Main',
+  components: {},
+  data () {
+    return {
+    }
+  },
+  created () {
+  },
+  methods: {}
+}
+</script>
+
+<style lang="css" scoped>
+#fm_notes {
+  white-space: pre-wrap; /* なんでダメなんだぁ。 */
+}
+</style>
